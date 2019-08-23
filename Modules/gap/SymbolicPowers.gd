58850b7d38f7dfb23c0841bd7a9f08926e172ff5
VInay Wagh <vinay_wagh@yahoo.com>
2019-08-24 02:44:24 +0530

    Added declaration for method for Join of 2 ideals

--- Modules/gap/SymbolicPowers.gd
+++ Modules/gap/SymbolicPowers.gd
@@ -8,17 +8,8 @@
 ##
 #############################################################################
 
-##  <#GAPDoc Label="SymbolicPower">
-##  <ManSection>
-##    <Oper Arg="I,n" Name="SymbolicPower"/>
-##    <Returns>a &homalg; module</Returns>
-##    <Description>
-##      For an ideal <A>I</A>, construct the <A>n</A>-th symbolic power of <A>I</A>.
-##    </Description>
-##  </ManSection>
-##  <#/GAPDoc>
-DeclareOperation( "SymbolicPower",
-        [ IsHomalgModule, IsInt ]);
+DeclareOperation( "SymbolicJoinOfIdeals",
+        [ IsHomalgModule, IsHomalgModule ]);
 
 DeclareOperation( "SymbolicPower",
         [ IsHomalgMatrix, IsInt ]);
